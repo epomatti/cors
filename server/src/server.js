d24f96de92cf9c7f1b4518a270ec53ff774fb042
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-08-05 22:05:49 -0300

    manual cors

--- server/src/server.js
+++ server/src/server.js
@@ -2,16 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+require('dotenv').config()
+const CORS_ENABLED = process.env.CORS_ENABLED;
+
 app.get('/hello', (req, res) => {
-  
-  res.send(`Hello, ${req.query.name}!`)
+  if (CORS_ENABLED) {
+    setCors(res);
+  }
+  response = {
+    msg: `Hello, ${req.query.name}!`
+  }
+  res.send(response)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-const setCors = (res) => {
+const setManualCors = (res) => {
   res.header("Access-Control-Allow-Origin", "*");
   // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
