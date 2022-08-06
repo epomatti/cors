175c2d2a0d890daa0f640372661ff33eaa522b67
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-08-05 21:46:47 -0300

    index

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/hello', (req, res) => {
+  
+  res.send(`Hello, ${req.query.name}!`)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+const setCors = (res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+}
